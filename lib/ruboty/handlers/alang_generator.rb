7d86273dc0ed0ce812561261a019fd7d50449ee6
cielavenir <cielartisan@gmail.com>
2014-12-04 18:36:12 +0900

    fix

--- lib/ruboty/handlers/alang_generator.rb
+++ lib/ruboty/handlers/alang_generator.rb
@@ -3,7 +3,7 @@ require "ruboty/alang_generator/actions/generate"
 module Ruboty
   module Handlers
     class AlangGenerator < Base
-      on /alang (?<text>.*?)/, name: 'alang', description: 'generate alang code'
+      on /alang (?<text>.*)/, name: 'alang', description: 'generate alang code'
 
       def alang(message)
         Ruboty::AlangGenerator::Actions::Generate.new(message).call
