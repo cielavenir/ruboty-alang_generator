9af3c08b12040701543129d05ed4afe24a594640
cielavenir <cielartisan@gmail.com>
2014-12-04 18:10:37 +0900

    first

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in ruboty-alang_generator.gemspec
+gemspec
---
+++ lib/ruboty/alang_generator.rb
@@ -0,0 +1,8 @@
+require "ruboty/alang_generator/version"
+require "ruboty/handlers/alang_generator"
+
+module Ruboty
+  module AlangGenerator
+    # Your code goes here...
+  end
+end
